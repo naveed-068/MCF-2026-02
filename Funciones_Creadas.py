05000fa4441f04028619b805d2e4693e7c4c6751
naveed-068 <naveed@ciencias.unam.mx>
2026-04-29 17:30:19 -0600

    Se agregan funciones para calcular el VaR y CVaR para el metodo Parametrico y el metodo Historico

--- Funciones_Creadas.py
+++ Funciones_Creadas.py
@@ -8,8 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
 import datetime as dt
-import scipy.stats
-from scipy.stats import kurtosis, skew, shapiro, norm
+from scipy import stats
+from scipy.stats import kurtosis, skew, shapiro, norm, t
 
 #Funcion para descargar los datos de las acciones.
 def descargar_datos(ticker, start_date, end_date):
@@ -57,8 +57,81 @@ def sesgo(rendimientos):
     Recordemos que el sesgo va a ser una medida de
 
     Args:
-        rendimientos (_type_): _description_
+        rendimientos (Dataframe): Los rendimientos diarios del activo financiero
     """
     sesg = skew(rendimientos)
     return sesg
+
+#VaR
+def Value_at_Risk(rendimientos, alpha):
+    """
+    Esta función se encargara de calcular el VaR de los rendimientos
+
+    Args:
+        alpha (float): El porcentaje del cuantil al que quieres llegar, es un numero entre 0 y 1 
+        
+        rendimientos (Dataframe): Los rendimientos diarios del activo financiero
+    """
+    if alpha < 1 and alpha > 0:
+        VaR = np.percentile(rendimientos, (1-alpha)*100)
+    else:
+        return "Hay que elegir un alpha en el intervalo (0,1)"
+    return VaR
+
+def Expected_Shortfall(rendimientos, alpha):
+    """
+    Esta función se encargará de calcular el Spected_Shortfall a un porcentaje alpha
+    de los rendimientos
+
+    Args:
+        rendimientos (Dataframe): Son los rendimientos del activo financiero
+        alpha (float): El porcentaje alpha al que se quiere calcular, es un número entre 0 y 1
+    """
+    if alpha > 0 and alpha < 1:
+        VaR = np.percentile(rendimientos, (1-alpha)*100)
+        CVaR = rendimientos[rendimientos <= VaR].mean()
+        return CVaR
+    else:
+        return "Hay que elegir un alpha que este entre (0,1)"
     
+
+def Expected_Shortfall_tstudent(alpha, df, loc, scale):
+
+    """
+    Función para calcular el Expected Shortfall de una distribución t-student
+    
+    Args:
+        alpha(float): Nivel de significancia
+        df(float): Grados de libertad
+        loc(float): Parámetro de locación
+        scale(float): Parámetro de escala
+    """
+    
+    # Valor crítico t para el alpha dado
+    t_alpha = t.ppf(alpha, df)
+    
+    # Función de densidad en t_alpha
+    pdf_t_alpha = t.pdf(t_alpha, df)
+    
+    # Fórmula paramétrica del ES para t-student
+    # ES = loc + scale * [ -f(t_alpha)/α * ((df + t_alpha^2)/(df - 1)) ]
+    ES_std = -pdf_t_alpha / alpha * ((df + t_alpha**2) / (df - 1))
+    ES = loc + scale * ES_std
+    
+    return ES
+
+def Expected_Shortfall_normal(alpha, mu, sigma):
+      """
+      Funcion para calcular el Expected Shortfall de una normal con media mu y varianza sigma
+
+      Args:
+          alpha (float): Alpha al que se quiere calcular el ES
+          mu (_type_): La media de la normal
+          sigma (_type_): La Varianza de la normal
+
+      Returns:
+          float: Se obtiene como resultado el CVaR 
+      """
+      z_alpha = stats.norm.ppf(alpha)  # Esto es el VaR
+      ES = mu - sigma * (stats.norm.pdf(z_alpha) / alpha) 
+      return ES
